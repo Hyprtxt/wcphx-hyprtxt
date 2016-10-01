600ac875f519384bf1068fdddf72028b20861fbc
Taylor Young <taylor@hyprtxt.com>
2016-10-01 10:22:31 -0700

    WordCamp Phoenix Plugin Init

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Hyprtxt Boilerplate
+Plugin URI: http://hyprtxt.com/
+Description: This is not a plugin, it's just a start.
+Author: Taylor Young
+Version: 1.0
+Author URI: http://hyprtxt.com/
+*/
+
+add_shortcode( 'boilerplate', function ( $atts ) {
+	$html = '';
+	return 'Boilerplate Shortcode';
+});
+
+// Add a body class
+add_filter( 'body_class', function ( $classes ) {
+	if ( is_front_page() ) :
+		$classes[] = 'hyprtxt';
+		return $classes;
+	else :
+		return $classes;
+	endif;
+});
+
+require_once( plugin_dir_path( __FILE__ ) . '/includes/CPT.php' );
